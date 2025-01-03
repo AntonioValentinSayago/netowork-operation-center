d1ea44669c27710f0ab6dafbed5c5072114c3f85
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-01-02 19:42:41 -0600

    fix(update-file) : Update server.ts with case-check-service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 export class Server {
@@ -6,8 +7,12 @@ export class Server {
         CronService.createJob(
             '*/5 * * * * *', // cronTime
             () => {
-                const date = new Date()
-                console.log(`You will see this message ${date} second`);
+                // const date = new Date()
+                // console.log(`You will see this message ${date} second`);
+                new CheckService(
+                    () => console.log('success'),
+                    ( error ) => console.log(error)
+                ).execute( 'http://localhost:3000' )
             }
         )
     }
