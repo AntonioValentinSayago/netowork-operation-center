53f8c418c80b558c45ed9465883b0c880bd43682
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-01-08 20:30:37 -0600

    fix(implementation) : create instancia filesSystemLogRepository

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasources";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log-impl.repository";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemlogRepository = new LogRepositoryImpl( 
+    new FileSystemDatasource()
+ )
 
 export class Server {
     static start() {
@@ -9,7 +15,9 @@ export class Server {
             () => {
                 // const date = new Date()
                 // console.log(`You will see this message ${date} second`);
+                // --> const url = 'https://google.com';
                 new CheckService(
+                    fileSystemlogRepository,
                     () => console.log('success'),
                     ( error ) => console.log(error)
                 ).execute( 'http://localhost:3000' )
