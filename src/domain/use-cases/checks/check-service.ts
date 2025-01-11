aa274c76d9522f05dd9ee8a3df849deb9d380834
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-01-11 00:26:53 -0600

    Fix: Solucion del bug a la refactorizacion del codigo

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -2,35 +2,35 @@ import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
 import { LogRepostory } from "../../repository/log.repository";
 
 interface CheckServiceUseCase {
-    execute( url: string ) : Promise<boolean>
+    execute(url: string): Promise<boolean>
 }
 
-type SuccessCallback = (( ) => void) | undefined;
-type ErrorCallback = (( error: string ) => void) | undefined;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
-export class CheckService implements CheckServiceUseCase{
+export class CheckService implements CheckServiceUseCase {
 
     constructor(
         private readonly logRepository: LogRepostory,
         private readonly successCallback: SuccessCallback,
-        private readonly errorCallback : ErrorCallback
-    ){}
+        private readonly errorCallback: ErrorCallback
+    ) { }
 
-    async execute( url: string ) : Promise<boolean> {
+    async execute(url: string): Promise<boolean> {
 
         try {
-            const req = await fetch( url );
-            if( !req.ok ) throw new Error("Error on check service");
+            const req = await fetch(url);
+            if (!req.ok) throw new Error("Error on check service");
 
-            const log = new LogEntity(`Service Working`, LogSeverityLevel.low)
-            this.logRepository.saveLog( log )
+            const log = new LogEntity({ message: `Service Working`, level: LogSeverityLevel.low, origin: 'check-service.ts' })
+            this.logRepository.saveLog(log)
             this.successCallback && this.successCallback()
             return true;
         } catch (error) {
-            const errorMessage = `${ error }`
-            const log = new LogEntity( errorMessage, LogSeverityLevel.high)
-            this.logRepository.saveLog( log )
-            this.errorCallback && this.errorCallback( errorMessage )
+            const errorMessage = `${error}`
+            const log = new LogEntity({ message: errorMessage, level: LogSeverityLevel.high, origin: 'check-service.ts' })
+            this.logRepository.saveLog(log)
+            this.errorCallback && this.errorCallback(errorMessage)
             return false;
         }
 
